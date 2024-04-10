4f2fbdde540d6532aa739f9334793559b6d06fd2
kcnabin <kcnabinkc@gmail.com>
2024-04-10 18:54:03 +0545

    project images added

--- src/components/PillInfo.jsx
+++ src/components/PillInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PillInfo = ({ text }) => {
   return (
-    <div className="border px-2 py-1 bg-secondary fw-bold text-white rounded-pill font-12">
+    <div className="border px-3 py-1 bg-secondary fw-bold text-white rounded-pill font-12 text-center">
       {text}
     </div>
   )
--- src/projects/ProjectSection.jsx
+++ src/projects/ProjectSection.jsx
@@ -37,10 +37,67 @@ const ProjectSection = () => {
     },
     {
       id: 1,
+      title: 'Money Manager React Native',
+      stack: 'React Native & SQLite',
+      type: 'Mobile App (Android/iOS)',
+      photos: [
+        '/images/projects/money-manager-react-native/category-line-chart.png',
+        '/images/projects/money-manager-react-native/day-tab.png',
+        '/images/projects/money-manager-react-native/expenses-summary-pie-chart.png',
+        '/images/projects/money-manager-react-native/add-transaction-form.png',
+        '/images/projects/money-manager-react-native/native-date-selector.png',
+        '/images/projects/money-manager-react-native/month-tab.png',
+        '/images/projects/money-manager-react-native/calendar-tab.png',
+        '/images/projects/money-manager-react-native/selected-date-transactions.png',
+        '/images/projects/money-manager-react-native/month-selector-modal.png',
+        '/images/projects/money-manager-react-native/all-categories.png',
+        '/images/projects/money-manager-react-native/add-category-form.png',
+        '/images/projects/money-manager-react-native/native-alert.png',
+        '/images/projects/money-manager-react-native/search-screen.png',
+        '/images/projects/money-manager-react-native/update-transaction-screen.png',
+        '/images/projects/money-manager-react-native/empty-summary.png',
+        '/images/projects/money-manager-react-native/empty-day-tab.png',
+        '/images/projects/money-manager-react-native/about-screen.png',
+
+      ],
+      github: 'https://github.com/kcnabin/money-manager-react-native',
+      details: {
+        frontend: 'React Native, Redux Toolkit, React Navigation',
+        backend: 'N/A',
+        database: 'SQLite',
+        projectInfo: [
+          'A React Native App for tracking all of your expenses and incomes'
+        ]
+      }
+    },
+    {
+      id: 2,
       title: 'Camping Nepal',
       stack: 'MERN',
       type: 'Booking System',
-      photos: ['/images/projects/camping-nepal/0.png'],
+      photos: [
+        '/images/projects/camping-nepal/1-index-page-mobile.png',
+        '/images/projects/camping-nepal/2-navigation-mobile.png',
+        '/images/projects/camping-nepal/3-booking-page-1-mobile.png',
+        '/images/projects/camping-nepal/4-booking-page-2-mobile.png',
+        '/images/projects/camping-nepal/5-add-camp-1-mobile.png',
+        '/images/projects/camping-nepal/6-add-camp-2-mobile.png',
+        '/images/projects/camping-nepal/7-my-places-mobile.png',
+        '/images/projects/camping-nepal/8-my-booking-mobile.png',
+        '/images/projects/camping-nepal/9-booking-page-place-photos-mobile.png',
+        '/images/projects/camping-nepal/10-booking-confirmed.png',
+        '/images/projects/camping-nepal/11-customer-booking-mobile.png',
+        '/images/projects/camping-nepal/12-register-mobile.png',
+        '/images/projects/camping-nepal/13-login-mobile.png',
+        '/images/projects/camping-nepal/14-my-places-empty-mobile.png',
+        '/images/projects/camping-nepal/15-search-mobile.png',
+        '/images/projects/camping-nepal/16-profile-mobile.png',
+        '/images/projects/camping-nepal/17-update-profile-mobile.png',
+        '/images/projects/camping-nepal/18-update-password-mobile.png',
+        '/images/projects/camping-nepal/19-homepage-desktop.png',
+        '/images/projects/camping-nepal/20-my-camps-desktop.png',
+        '/images/projects/camping-nepal/21-booking-page-desktop.png',
+      ],
       github: 'https://github.com/kcnabin/camping-nepal',
       details: {
         frontend: 'React, React Router, Bootstrap 5',
@@ -55,23 +112,7 @@ const ProjectSection = () => {
         ]
       }
 
-    }, {
-      id: 2,
-      title: 'Portfolio',
-      stack: 'Frontend',
-      type: 'Portfolio',
-      photos: ['/images/projects/portfolio/0.png'],
-      github: 'https://github.com/kcnabin/kcnabin.github.io',
-      details: {
-        frontend: 'React, React Router, Bootstrap 5',
-        backend: 'N/A',
-        database: 'N/A',
-        projectInfo: [
-          'Portfolio site showcasing my skills and projects',
-          `Since you're already here, what do you think?`
-        ]
-      }
-    }
+    },
   ]
   return (
     <div className='mt-5 pt-4'>
